102b3c95bdfd1b6fd11a98f3369106500db2c4ad
Naseef Sami <cmnsami@gmail.com>
2019-12-03 15:46:38 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
 # Add your variables here
+
+
+first_number = 42
+second_number = 25
+sum = first_number+second_number
+difference = first_number-second_number
+product = first_number*second_number
+quotient = first_number/second_number
